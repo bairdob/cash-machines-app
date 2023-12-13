814c94e18e8635ceb032d2d270c4ac8288bef6fa
bairdob <krumane@mail.ru>
2023-12-13 20:33:18 +0300

    добавил поиск оптимального пути брутфорсом

--- src/tsp_service.py
+++ src/tsp_service.py
@@ -1,3 +1,6 @@
+from itertools import permutations
+
+
 class TSPService:
     def __init__(self, matrix):
         self.matrix = matrix
@@ -94,6 +97,27 @@ class TSPService:
 
     def get_path_length(self):
         return self.path_length
+
+    def tsp_brute_force(self):
+        num_vertices = len(self.matrix)
+        min_distance = float('inf')
+        optimal_path = []
+
+        # генерация всех перестановок вершин
+        all_permutations = permutations(range(1, num_vertices))
+
+        for path in all_permutations:
+            path = (0,) + path + (0,)  # начало и конец в точке 0
+
+            distance = sum(self.matrix[path[i]][path[i + 1]] for i in range(num_vertices))
+
+            if distance < min_distance:
+                min_distance = distance
+                optimal_path = path
+
+        self.route = [i+1 for i in optimal_path[:-1]]
+
+        return min_distance, optimal_path
 
 
 if __name__ == '__main__':
