6fcb2a7dc2611ee2908f1cb5ceb15e8c0a6683b1
bairdob <krumane@mail.ru>
2023-12-14 16:22:10 +0300

    убрал лишние модели

--- src/db_models.py
+++ src/db_models.py
@@ -1,35 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Float
 
-from src.wmts.database import Base
-
-
-class ATM(Base):
-    __tablename__ = 'atm'
-
-    atm_id = Column(Integer, primary_key=True)
-    location_name = Column(String(100))
-    latitude = Column(Float)
-    longitude = Column(Float)
-    address = Column(String(255))
-    city = Column(String(100))
-    state = Column(String(50))
-    country = Column(String(50))
-    is_operational = Column(Boolean)
-    last_service_date = Column(Date)
-
-
-class ATMStatistics(Base):
-    __tablename__ = 'atm_statistics'
-
-    stats_id = Column(Integer, primary_key=True)
-    atm_id = Column(Integer, ForeignKey('atm.atm_id'))
-    transaction_date = Column(Date)
-    total_transactions = Column(Integer)
-    successful_transactions = Column(Integer)
-    failed_transactions = Column(Integer)
-
-    # Establish a relationship with the ATM table
-    # atm = relationship("ATM", back_populates="atm_statistics")
+from src.utils import Base
 
 
 class Locations(Base):
