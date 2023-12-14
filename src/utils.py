7148a13109e531ac9c43b2fe8f12a3b3e65405c5
bairdob <krumane@mail.ru>
2023-12-14 16:22:34 +0300

    рефактор

--- src/utils.py
+++ src/utils.py
@@ -1,49 +1,21 @@
-from typing import Tuple
-
-from sqlalchemy import select, func
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
-from src.wmts.database import engine, SessionLocal
-from src.db_models import Statistics
-from src.schemas import StatisticsNormalized
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/barbie_land"
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def map_statistics_to_statistics_normalized(statistics, max_priority, min_priority):
-    StatisticsNormalized.max_priority = max_priority
-    StatisticsNormalized.min_priority = min_priority
-    result = []
-    for statistic in statistics:
-        result.append(
-            StatisticsNormalized(
-                atm_id=statistic.atm_id,
-                services_per_day=statistic.services_per_day,
-                amount_per_day=statistic.amount_per_day,
-            )
-        )
-    sorted_result = sorted(
-        result,
-        key=lambda stat: stat.priority,
-        reverse=True,
-    )
-    return sorted_result
-
-
-def get_max_min_priority() -> Tuple[float, float]:
-    with engine.connect() as connection:
-        query_max_pr = select(func.max(Statistics.amount_per_day / Statistics.services_per_day))
-        query_min_pr = select(func.min(Statistics.amount_per_day / Statistics.services_per_day))
-
-        max_priority = connection.execute(query_max_pr).fetchone()
-        min_priority = connection.execute(query_min_pr).fetchone()
-
-    return max_priority[0], min_priority[0]
+Base = declarative_base()
 
 
 def get_db() -> Session:
     """
     Менеджер контекста для создания и управления сеансом базы данных.
 
-    :return: сеанс базы данных
+    :return: Сеанс базы данных
     """
     db = SessionLocal()
     try:
